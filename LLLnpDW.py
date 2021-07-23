df254074687b92c28130d2be6903126ef838c404
VelascoL <51166105+VelascoL@users.noreply.github.com>
2021-07-23 15:23:00 -0400

    update

--- LLLnpDW.py
+++ LLLnpDW.py
@@ -18,99 +18,18 @@ from fpylll import *
 import operator
 
 
-def generate_lattice_approx(large_constant,primes):
-    """
-    Generates the lattice approximation matrix not using fyplll
-    """
-    size = len(primes)
-    
-    approximation_matrix = np.identity(size-1)
-    # Append an empty column to the identity matrix
-    approximation_matrix = np.concatenate((approximation_matrix,np.zeros((size-1,1))),axis = 1)
-    # Create the row that represents the approximation of the linear form
-    row_approx = []
-    for i in range(len(primes)):
-        #if i == len(primes)-1:
-         #   row_approx.append(-math.floor(Fraction(large_constant) * Fraction(math.log(primes[i]))))
-        #else:
-        row_approx.append(np.rint(Fraction(large_constant) * Fraction(math.log(primes[i]))))
-    row_approx = np.reshape(row_approx,(1,size))
-    print(np.shape(approximation_matrix))
-    print(np.shape(row_approx), np.shape(approximation_matrix))
-    approximation_matrix = np.concatenate((approximation_matrix, row_approx),axis = 0)
-    return approximation_matrix
-
 def generate_lattice(large_constant, eta_list):
     size = len(eta_list)
     lattice = np.identity(size-1)
     lattice = np.concatenate((lattice, np.zeros((size-1,1))),axis=1)
     row = []
-    for i in range(len(primes)):
+    for i in range(len(eta_list)):
         row.append(math.floor(Fraction(large_constant) * Fraction(math.log(eta_list[i]))))
     print(row)
     row = np.reshape(row,(1,size))
     lattice = np.concatenate((lattice,row),axis=0)
     return lattice
     
-                   
-
-def projection_scale(u, v):
-    '''Computes <u,v>/<u,u>, which is the scale used in projection.'''
-    return np.dot(u, v) / np.dot(u, u)
-
-def proj(u, v):
-    '''Computes the projection of vector v onto vector u. Assumes u is not zero.'''
-    print(projection_scale(u,v))
-    return projection_scale(u,v)*u
-
-def gram_schmidt(basis): 
-    '''Computes Gram Schmidt orthoganalization (without normalization) of a basis.'''
-    orthobasis = basis.copy()
-    orthobasis[:,0] = basis[:,0]
-    mu = np.empty((np.shape(basis)))
-    for i in range(1, np.shape(basis)[1]):  # Loop through dimension of basis.
-        orthobasis[:,i] = basis[:,i] 
-        for j in range(0, i):
-            orthobasis[:,i] -= proj(orthobasis[:,j], basis[:,i])
-            mu[i][j] = projection_scale(orthobasis[:,j],basis[:,i])
-    return orthobasis,mu
-
-def initialize(basis):
-    D = get_D(basis)
-    GS,mu = gram_schmidt(basis)
-    Lambda = basis.copy()
-    for i in range(np.shape(basis)[0]):
-        for j in range(np.shape(basis)[1]):
-            Lambda[i][j] = D[j]*mu[i][j]
-    return Lambda,D
-    
-    
-
-def get_D(basis):
-    D = np.empty((np.shape(basis)[0]+1,1),dtype=np.float64)
-    ortho = basis.copy()
-    GS = gram_schmidt(basis)[0]  
-    #print(GS)
-    for i in range(np.shape(GS)[0]+1):
-        if i == 0:
-            D[i] = 1
-        else:
-            D[i] = reduce(operator.mul,[np.linalg.norm(GS[:,j])**2 for j in range(i)])
-    print(D)
-    return D
-
-#===================================================
-
-import sys
-import json
-import numpy as np
-from numpy import linalg as la
-
-#k = 1 # Initialize the working index.
-#DELTA = 0.75 
-
-
-
 def initial(basis):
     D = []
     D.append(1)
@@ -121,11 +40,10 @@ def initial(basis):
     for i in range(0,np.shape(basis)[0]):
         casis[:,i] = basis[:,i]
         for j in range(0,i):
+            print(i,j,basis.column(i), casis.column(j))
             Lambda[i,j] = basis.column(i).dot_product(casis.column(j))
             casis[:,i] = (D[j+1]*casis.column(i) - Lambda[i][j]*casis.column(j))/D[j]
-            print(casis)
         D.append((casis.column(i).dot_product(casis.column(i)))/D[i])
-    print('D',D)
     D = vector(ZZ,D)
     Lambda = Matrix(ZZ,Lambda)
     return Lambda,D
@@ -138,7 +56,6 @@ def prod_A(k,l,basis,Lambda,D):
         for j in range(0,l-1):
             Lambda[k-1,j] = Lambda[k-1,j] - r*Lambda[l-1,j]
         Lambda[k-1,l-1] = Lambda[k-1,l-1] - r*D[l]
-    print('a',Lambda)
     return k,l,basis,Lambda,D
         
 def prod_B(k,basis,Lambda,D):
@@ -147,43 +64,75 @@ def prod_B(k,basis,Lambda,D):
         Lambda[[k-2,k-1],j] = Lambda[[k-1,k-2],j]
     for i in range(k,np.shape(basis)[0]):
         t = Lambda[i,k-2]
-        s = (Lambda[i,k-2]*Lambda[k-1,k-2] + Lambda[i,k-1]*D[k-2])/D[k-1]
-        print(s.is_integer())
         Lambda[i,k-2] = (Lambda[i,k-2]*Lambda[k-1,k-2] + Lambda[i,k-1]*D[k-2])/D[k-1]
         Lambda[i,k-1] = (t*D[k] - Lambda[i,k-1]*Lambda[k-1,k-2])/D[k-1]
     D[k-1] = (D[k-2]*D[k] + Lambda[k-1,k-2]**2)/D[k-1]
     return k,basis,Lambda,D
     
-def main(basis):
+def LLLDW(basis):
     Lambda,D = initial(basis)
+    print('basis',basis)
     print('L',Lambda)
     print('D', D)
     k=2
     while k <= np.shape(basis)[0]: 
         l = k-1
-        print('doing prod A')
+        print('doing prod A1')
         k,l,basis,Lambda,D = prod_A(k,l,basis,Lambda,D)
         print(Lambda)
         if 4*D[k-2]*D[k] < (3*D[k-1]**2 - 4*Lambda[k-1,k-2]**2):
             print('doing prod B',k)
             k,basis,Lambda,D = prod_B(k,basis,Lambda,D)
-            print('D',D)
+            print(Lambda,D)
             if k > 2:
                 k = k-1
         else:
             print('doing the other thing',k)
             for l in range(k-2,2):
-                print('doing prod A')
+                print('doing prod A2')
                 k,l,basis,Lambda,D = prod_A(k,l,basis,Lambda,D)
-                #print(Lambda)
+                print(Lambda)
             k = k + 1
+    print(basis)
+    print(Lambda)
+    print(D)
     return basis
+
+def calculate_norm_squared(v):
+    return v.dot_product(v)
+
+def calculate_max_frac_norm_squared(lll_reduced_basis, gs_basis):
+    return max([calculate_norm_squared(lll_reduced_basis.column(0))/calculate_norm_squared(gs_basis.column(i)) for i in range(size)])
+
+def c1(red):
+    red = red.transpose()
+    gs,mu = red.gram_schmidt()
+    print(gs)
+    c1 = calculate_max_frac_norm_squared(red,gs)
+    return c1
+
+def c4(c1,basis):
+    b1 = basis.column(0)
+    b1sq = calculate_norm_squared(b1)
+    c4 = (c1**(-1))*b1sq
+    return c4
+        
     
 if __name__ == "__main__":
-    large_constant = 10**(10)
+    large_constant = 10**(100)
     primes = [2,3,5]
+    size = len(primes)
     basis = Matrix(ZZ,generate_lattice(large_constant,primes))
-    print(main(basis))
-
+    #print(initial(basis))
+    red = LLLDW(basis)
+    print(red[:,0])
+    #c_1 = c1(red)
+    #c_4 = c4(c_1,red)
+    #S = 154*2
+    #T = (1 + 154*3)/2
+    #if c_4 >= T**2 + S:
+    #    print('yep')
+    
+    
 
 
